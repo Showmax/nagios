8e50255336a6348c139322023b09b90250a45ae9
Zdenek Styblik <zdenek.styblik@icflix.com>
2014-11-04 17:48:41 +0100

    Sender - clean up Python coding style

--- nagios_sender.py
+++ nagios_sender.py
@@ -4,7 +4,7 @@ Copyright (c) 2014, ICFLIX Media FZ LLC All rights reserved.
 Use of this source code is governed by a BSD-style license that can be
 found in the LICENSE file.
 
-Desc: Take whatever is given on STDIN, "encrypt it", HTTP POST it to the server
+Desc: Execute % check_multi;, "encrypt" its STDOUT, HTTP POST it to the server
 """
 import argparse
 import base64
@@ -48,14 +48,14 @@ CMD_GET_RESULTS = [CHECK_MULTI_BIN,
 
 # MyPie {{{
 class MyPie(pyinotify.ProcessEvent):
-    """ Class to Process Inotify Events """
+    """Class to Process Inotify Events."""
 
     def __init__(self, mqueue):
         pyinotify.ProcessEvent.__init__(self)
         self.mqueue = mqueue
 
     def _enqueue(self, event_desc, filename):
-        """ Enqueue events for MainThread """
+        """Enqueue events for MainThread."""
         try:
             self.mqueue.put((event_desc, filename))
         except Queue.Full:
@@ -63,38 +63,38 @@ class MyPie(pyinotify.ProcessEvent):
             pass
 
     def process_IN_DELETE(self, event):
-        """ Deleted """
+        """Deleted."""
         self._enqueue('deleted', os.path.join(event.path, event.name))
 
     def process_IN_CREATE(self, event):
-        """ Created """
+        """Created."""
         self._enqueue('created', os.path.join(event.path, event.name))
 
     def process_IN_MODIFY(self, event):
-        """ Modified """
+        """Modified."""
         self._enqueue('modified', os.path.join(event.path, event.name))
 
     def process_IN_ATTRIB(self, event):
-        """ Attribute change """
+        """Attribute change."""
         self._enqueue('attr_change', os.path.join(event.path, event.name))
 
     def process_IN_MOVED_FROM(self, event):
-        """ Moved from somewhere """
+        """Moved from somewhere."""
         self._enqueue('moved_from', os.path.join(event.path, event.name))
 
     def process_IN_CLOSE_WRITE(self, event):
-        """ Written to """
+        """Written to."""
         self._enqueue('written', os.path.join(event.path, event.name))
 
     def process_IN_MOVED_TO(self, event):
-        """ Moved to """
+        """Moved to."""
         self._enqueue('moved_to', os.path.join(event.path, event.name))
 
 
 # }}}
 # NagiosRunit {{{
 class NagiosRunit(object):
-    """ Nagios Client/Node executed from runit """
+    """Nagios Client/Node executed from runit."""
 
     def __init__(self):
         self.inotifier = None
@@ -109,7 +109,7 @@ class NagiosRunit(object):
         self.url = None
 
     def _stop_inotifier(self):
-        """ Stop inotify thread """
+        """Stop inotify thread."""
         try:
             self.inotifier.stop()
             self.inotifier.join(5)
@@ -117,11 +117,11 @@ class NagiosRunit(object):
             pass
 
     def handler_signal(self, signum, frame):
-        """ Handle signals, resp. set event """
+        """Handle signals, resp. set event."""
         self._stop.set()
 
     def run(self):
-        """ Main """
+        """Main."""
         signal.signal(signal.SIGHUP, self.handler_signal)
         signal.signal(signal.SIGINT, self.handler_signal)
         signal.signal(signal.SIGTERM, self.handler_signal)
@@ -138,7 +138,7 @@ class NagiosRunit(object):
         self.stop()
 
     def send_results(self):
-        """ Send results to remote Nagios Host """
+        """Send results to remote Nagios Host."""
         sender = NagiosSender()
         sender.set_command(CMD_GET_RESULTS)
         sender.set_url('%s%s' % (self.url, self.results_uri))
@@ -152,14 +152,14 @@ class NagiosRunit(object):
         del sender
 
     def send_config(self):
-        """ Send updates done to CHECK_MULTI_DIR to remote Host """
+        """Send updates done to CHECK_MULTI_DIR to remote Host."""
         if self.mqueue.qsize() < 1:
             return
 
         while True:
             try:
                 (event_desc, fpath) = self.mqueue.get(1, True)
-                logging.info("Event %s on file %s", event_desc, fpath)
+                logging.info('Event %s on file %s', event_desc, fpath)
             except Queue.Empty:
                 break
 
@@ -176,31 +176,31 @@ class NagiosRunit(object):
         del sender
 
     def set_config_uri(self, config_uri):
-        """ Set URI for posting Config """
+        """Set URI for posting Config."""
         self.config_uri = config_uri
 
     def set_environment(self, environment):
-        """ Set working environment """
+        """Set working environment."""
         self.environment = environment
 
     def set_interval(self, interval):
-        """ Set interval for sending results """
+        """Set interval for sending results."""
         self.interval = interval
 
     def set_results_uri(self, results_uri):
-        """ Set URI for posting Results """
+        """Set URI for posting Results."""
         self.results_uri = results_uri
 
     def set_shared_key(self, shared_key):
-        """ Set shared key for scrambling data """
+        """Set shared key for scrambling data."""
         self.shared_key = shared_key
 
     def set_url(self, url):
-        """ Set Nagios Host URL """
+        """Set Nagios Host URL."""
         self.url = url
 
     def stop(self):
-        """ Stop everything """
+        """Stop everything."""
         self._stop.set()
         self._stop_inotifier()
 
@@ -208,7 +208,7 @@ class NagiosRunit(object):
 # }}}
 # NagiosSender {{{
 class NagiosSender(object):
-    """ Nagios Client/Node class - accept STDIN, encrypt and HTTP POST it """
+    """Nagios Client/Node class - accept STDIN, encrypt and HTTP POST it."""
 
     def __init__(self):
         self.command = None
@@ -217,18 +217,18 @@ class NagiosSender(object):
         self.http_timeout = 15
 
     def encode(self, key, string):
-        """ Encrypt given string with given key """
+        """Encrypt given string with given key."""
         encoded_chars = []
         for i in xrange(len(string)):
             key_c = key[i % len(key)]
             encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
             encoded_chars.append(encoded_c)
 
-        encoded_string = "".join(encoded_chars)
+        encoded_string = ''.join(encoded_chars)
         return base64.urlsafe_b64encode(encoded_string)
 
     def decode(self, key, string):
-        """ Try to decrypt given string with given key """
+        """Try to decrypt given string with given key."""
         decoded_chars = []
         string = base64.urlsafe_b64decode(string)
         for i in xrange(len(string)):
@@ -240,8 +240,8 @@ class NagiosSender(object):
         return decoded_string
 
     def run(self):
-        """ Go, go, go! """
-        logging.debug('Command %s', " ".join(self.command))
+        """Go, go, go!"""
+        logging.debug('Command %s', ' '.join(self.command))
         stdin = self.run_command(self.command)
         if stdin is None:
             logging.error('Command %s has returned empty STDIN!',
@@ -274,7 +274,7 @@ class NagiosSender(object):
         del rsp
 
     def run_command(self, cmd):
-        """ Run given command and return its STDOUT """
+        """Run given command and return its STDOUT."""
         try:
             proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         except Exception as exception:
@@ -293,46 +293,34 @@ class NagiosSender(object):
         return stdout_lines
 
     def set_command(self, command):
-        """ Set command to execute - list is expected """
+        """Set command to execute - list is expected."""
         self.command = command
 
     def set_shared_key(self, shared_key):
-        """ Set shared key for scrambling message """
+        """Set shared key for scrambling message."""
         self.shared_key = shared_key
 
     def set_url(self, url):
-        """ Set URL we're going to POST to """
+        """Set URL we're going to POST to."""
         self.url = url
 
 
 # }}}
 def get_actions():
-    """ Return list of available actions """
+    """Return list of available actions."""
     return ['runit', 'send_config', 'send_results']
 
 def get_environments():
-    """ Return list of supported environments """
+    """Return list of supported environments."""
     return ['production', 'staging', 'development']
 
 def main():
-    """ main function - setup logging and launch instance of NagiosSender """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-a', dest='action', type=str, help='action to do',
-            choices=get_actions())
-    parser.add_argument('-e', dest='environment', type=str, help='environment',
-            choices=get_environments(), default=DEFAULT_ENVIRONMENT)
-    parser.add_argument('-i', dest='interval', type=int,
-            help='how often to send check results', default=DEFAULT_INTERVAL)
-    parser.add_argument('-v', dest='verbose', action='store_true',
-            help='add to logging verbosity')
-    args = parser.parse_args()
-    # Set up logging.
-    log_level = logging.INFO
-    if args.verbose:
-        log_level = logging.DEBUG
-
+    """Main function - setup logging and launch instance of NagiosSender."""
     logging.basicConfig(format=LOG_FORMAT, stream=sys.stdout)
-    logging.getLogger().setLevel(log_level)
+    logging.getLogger().setLevel(logging.INFO)
+    args = parse_cli_args()
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
 
     # Yes, we want to crash loud if some of those aren't provided
     shared_key = os.environ['NAGIOS_SHARED_KEY']
@@ -368,6 +356,24 @@ def main():
         nagios_runit.run()
 
     logging.shutdown()
+
+def parse_cli_args():
+    """Return parsed CLI args."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a',
+            dest='action', type=str, choices=get_actions(),
+            help='Action to do.')
+    parser.add_argument('-e',
+            dest='environment', type=str, choices=get_environments(),
+            default=DEFAULT_ENVIRONMENT,
+            help='Set environment.')
+    parser.add_argument('-i',
+            dest='interval', type=int, default=DEFAULT_INTERVAL,
+            help='How often to send check results.')
+    parser.add_argument('-v',
+            dest='verbose', action='store_true',
+            help='Increase logging verbosity.')
+    return parser.parse_args()
 
 if __name__ == '__main__':
     main()
